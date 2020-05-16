512d53a486cd167a334cf6a6d34e2a36d5b071b9
Monis Siddiqui <monis@Monis-Macbook-Pro.local>
2020-05-16 11:51:40 -0400

    Complete 8.01

--- epi_judge_python/stack_with_max.py
+++ epi_judge_python/stack_with_max.py
@@ -1,22 +1,34 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+from collections import namedtuple
+from typing import List
+
 
 class Stack:
+    ElementWithMax = namedtuple("ElementWithMax", ["element", "max"])
+
+    def __init__(self):
+        self.stack: List[Stack.ElementWithMax] = []
+
     def empty(self) -> bool:
-        # TODO - you fill in here.
-        return True
+        return len(self.stack) == 0
 
     def max(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        # if self.empty():
+        #     raise IndexError('max(): empty stack')
+        return self.stack[-1].max #if not self.empty() else
 
     def pop(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        # if self.empty():
+        #     raise IndexError('pop(): empty stack')
+        return self.stack.pop().element
 
     def push(self, x: int) -> None:
-        # TODO - you fill in here.
+        self.stack.append(self.ElementWithMax(
+            x, x if self.empty() else max(
+                x, self.max()
+            )))
         return
 
 
