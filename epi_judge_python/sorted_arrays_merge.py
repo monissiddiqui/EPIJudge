4eb59fd6dbd8909101570129fd727468a583ffc8
Monis Siddiqui <monis@Monis-Macbook-Pro.local>
2020-05-29 23:53:52 -0400

    Completed 10.01

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,11 +1,28 @@
 from typing import List
+import heapq
+from queue import Queue
+from collections import deque
 
 from test_framework import generic_test
 
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+
+    merged = deque([],sum([len(arr) for arr in sorted_arrays]))
+    minHeap = []
+    for i in range(len(sorted_arrays)) :
+        heapq.heappush(minHeap, (sorted_arrays[i][0],i))
+    indices = [1] * len(sorted_arrays)
+    while len(minHeap) :
+        val,iarr = minHeap[0]
+        if indices[iarr] < len(sorted_arrays[iarr]) :
+            heapq.heapreplace(minHeap,(sorted_arrays[iarr][indices[iarr]],iarr))
+            indices[iarr] += 1
+        else :
+            heapq.heappop(minHeap)
+        merged.append(val)
+
+    return list(merged)
 
 
 if __name__ == '__main__':
