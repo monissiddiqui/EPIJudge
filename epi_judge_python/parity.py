75929eea9b835761b16b4bd6533f57a4487b6456
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-10 07:31:51 -0400

    Redo 4.01

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -51,14 +51,14 @@ from test_framework import generic_test
 #     return bitCount
 
 # # Solution 3 that makes use of continually parallelizing XORing the bits in the number.
-# def parity(x: int) -> int:
-#     x ^= (x >> 32)
-#     x ^= (x >> 16)
-#     x ^= (x >> 8)
-#     x ^= (x >> 4)
-#     x ^= (x >> 2)
-#     x ^= (x >> 1)
-#     return x & 0x1
+def parity_XOR(x: int) -> int:
+    x ^= (x >> 32)
+    x ^= (x >> 16)
+    x ^= (x >> 8)
+    x ^= (x >> 4)
+    x ^= (x >> 2)
+    x ^= (x >> 1)
+    return x & 0x1
 
 
 # Solution 3A that makes use of continually parallelizing XORing the bits in the number and cachng 16 bit word results.
@@ -78,6 +78,20 @@ def parity(x: int) -> int:
     x ^= (x >> 16)
     return PARITY_LOOKUP_TABLE[x & (2 ** 16 - 1)]
 
+# # REDO problem
+# # compute the parity for the first 2**(i+1) bits in n
+# def computeParity(n: int, i: int) :
+#     while i >= 0 :
+#         n ^= n >> (2**i)
+#         i -= 1
+#     return n & 1
+#
+# LOOKUP_TABLE = [computeParity(i,8) for i in range(2**16)]
+#
+# def parity(x : int) -> int :
+#     x ^= x >> 32
+#     x ^= x >> 16
+#     return LOOKUP_TABLE[x & (2**16-1)]
 
 if __name__ == '__main__':
     exit(generic_test.generic_test_main('parity.py', 'parity.tsv', parity))
