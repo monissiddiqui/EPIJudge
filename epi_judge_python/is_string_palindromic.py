7f7fb2322d619373a286b7f6e0aa42a0c518baea
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-11 16:35:53 -0400

    Redo 6.00 wih negative indexing

--- epi_judge_python/is_string_palindromic.py
+++ epi_judge_python/is_string_palindromic.py
@@ -1,11 +1,16 @@
 from test_framework import generic_test
 
 
+# def is_palindromic(s: str) -> bool:
+#     for i in range(len(s)//2) :
+#         if s[i] != s[len(s)-1-i] : return False
+#     return True
+
+# redo problem
 def is_palindromic(s: str) -> bool:
     for i in range(len(s)//2) :
-        if s[i] != s[len(s)-1-i] : return False
+        if s[i] != s[~i]: return False
     return True
-
 
 if __name__ == '__main__':
     exit(
