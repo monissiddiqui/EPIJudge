03af23894da7d4530aad6b6d381b3ae7d847fa3a
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-11 18:10:52 -0400

    Redo 6.01

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -3,37 +3,63 @@ from enum import Enum
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+from collections import deque
 
+# def int_to_string(x: int) -> str:
+#     if x == 0: return '0'
+#     mapIntToString = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9"}
+#     digits = []
+#     sign = ""
+#     if x < 0:
+#         x *= -1
+#         sign += "-"
+#     while x > 0:
+#         digits.append(mapIntToString[x % 10])
+#         x //= 10
+#     digits.append(sign)
+#     return "".join(reversed(digits))
+#
+#
+# def string_to_int(s: str) -> int:
+#     mapStringDigitToInt = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
+#     sign = 1
+#     lastDigitIndex = 0
+#     if s[0] not in mapStringDigitToInt:
+#         sign = -1 if s[0] == "-" else 1
+#         lastDigitIndex += 1
+#
+#     number = 0
+#     tensFactor = 1
+#     for i in range(len(s) - 1, lastDigitIndex - 1, -1):
+#         number += tensFactor * mapStringDigitToInt[s[i]]
+#         tensFactor *= 10
+#     number *= sign
+#
+#     return number
+
+# redo Problems
 def int_to_string(x: int) -> str:
-    if x == 0: return '0'
+    sb = deque()
     mapIntToString = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9"}
-    digits = []
-    sign = ""
+    isNegative = False
     if x < 0:
-        x *= -1
-        sign += "-"
-    while x > 0:
-        digits.append(mapIntToString[x % 10])
+        isNegative = True
+        x = -x
+    while x > 0 :
+        sb.appendleft(mapIntToString[x%10])
         x //= 10
-    digits.append(sign)
-    return "".join(reversed(digits))
-
+    if isNegative: sb.appendleft("-")
+    return ''.join(sb) if sb else "0"
 
 def string_to_int(s: str) -> int:
-    mapStringDigitToInt = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
-    sign = 1
-    lastDigitIndex = 0
-    if s[0] not in mapStringDigitToInt:
-        sign = -1 if s[0] == "-" else 1
-        lastDigitIndex += 1
-
     number = 0
-    tensFactor = 1
-    for i in range(len(s) - 1, lastDigitIndex - 1, -1):
-        number += tensFactor * mapStringDigitToInt[s[i]]
-        tensFactor *= 10
-    number *= sign
-
+    chartoNum = {"0":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9}
+    for i in range(len(s)) :
+        if s[i] in chartoNum :
+            number *= 10
+            number += chartoNum[s[i]]
+    if s[0] == "-" :
+        number *= -1
     return number
 
 
@@ -45,7 +71,6 @@ def wrapper(x, s):
 
 
 if __name__ == '__main__':
-    print(int_to_string(-4176473))
     exit(
         generic_test.generic_test_main('string_integer_interconversion.py',
                                        'string_integer_interconversion.tsv',
