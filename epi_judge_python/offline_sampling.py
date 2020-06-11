65f3d0c6b88de64918158144555caf0bc7414f1b
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-11 08:31:57 -0400

    Complete 5.12

--- epi_judge_python/offline_sampling.py
+++ epi_judge_python/offline_sampling.py
@@ -7,11 +7,27 @@ from test_framework.random_sequence_checker import (
     compute_combination_idx, run_func_with_retries)
 from test_framework.test_utils import enable_executor_hook
 
+from random import randrange
 
 def random_sampling(k: int, A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    '''
+    All subsets should be equally likely. If there are N choose k subsets and each of them
+    should be equally likely. This means that each subset has an overall likelihood of
+    1/(math.comb(n,k) = (k*(k-1)*...*2*1/(n*(n-1)*...*(n-k+2)*(n-k+1).
 
+    Assuming each element from the array is chosen iteratively and independently from the previous
+    elements selected, a random subset can be constructed as follows:
+
+    Amongst the first n elements, k will need to be selected and each element is equally likely so
+
+    Then once that element is selected, any one of the remaining elements is equally likely to
+    selected as the next element in the array.
+    '''
+
+    i = 0
+    for i in range(k) :
+        r = randrange(i,len(A))
+        A[i], A[r] = A[r], A[i]
 
 @enable_executor_hook
 def random_sampling_wrapper(executor, k, A):
