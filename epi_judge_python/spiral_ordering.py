a847b37b4b36a2f6f92dbc5737dbed5c62f47573
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-11 15:58:44 -0400

    Complete 5.18 Spiral Matrix

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -2,10 +2,34 @@ from typing import List
 
 from test_framework import generic_test
 
+from collections import deque
 
 def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    M = len(square_matrix)
+    if M == 0:
+        return []
+    N = len(square_matrix[0])
+
+    arr = deque(maxlen=M*N)
+    k = min((M+1)//2,(N+1)//2)
+    for i in range(k) :
+        # top of layer i
+        arr.extend(square_matrix[i][i:N-i])
+        # right side of layer i
+        sideRows = range(i+1,M-i)
+        for j in sideRows:
+            arr.append(square_matrix[j][~i])
+
+    # bottom Row if separate from top, inserted in reverse
+        if (M-i) != i :
+            arr.extend(square_matrix[~i][N-1-i-1:i:-1])
+
+    # left row if seperate from right, inserted in reverse
+        if (N-i) != i:
+            for j in reversed(sideRows):
+                arr.append(square_matrix[j][i])
+
+    return list(arr)
 
 
 if __name__ == '__main__':
