f8ec30c0c71d60082e3cdbbf0af586cffd4110ac
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-14 12:46:34 -0400

    Redo 9.01 and variants

--- epi_judge_python/9.01_variants.py
+++ epi_judge_python/9.01_variants.py
@@ -1,7 +1,7 @@
 import binary_tree_node
 
 '''
-Variant: Write a program that retums the size of the largest subtree that is complete.
+Variant: Write a program that returns the size of the largest subtree that is complete.
 
 Variant: Define a node in a binary tree to be k-balanced if the difference in the number of nodes in its left and right 
 subtrees is no more than k. Design an algorithm that takes as input a binary tree and positive integer k, and retums a 
@@ -15,12 +15,17 @@ def largest_complete_subtree_size(tree: binary_tree_node) -> int :
     :param tree:
     :return:
 
-    At each node, check if the left and right subtrees are complete and return the size
-    return the node as benig complete if one of following occurs:
-     1) left subtree is complete and the right subtree is perfect with height one less than left
-     2) left subtree is perfect and right subtree is complete with heights the same
-    unless both subtrees are perfect, return the current node as perfect and the height being the max
-    of the child trees.
+    This is a post-rder traversal since the node will have to know about the subtree's
+    (number of nodes, height, and completeness) the recursive funciton on a node returns as (1,0,True);
+    THen the status of each child is gathered. The node is returned as complete in any of
+    the following two scenarios where both subtrees are complete:
+        1. if left subtree is perfect (n = 2^(h+1)-1) and right subtree height is equal
+        2. o/w if right subtree is perfect and left subtree height is one greater
+    In either case (l.n + r.n + 1, max(l.h,r.h) +1, true) is returned and maxComplete
+    subtree size is updated
+
+    The node's subtree is incomplete in all other cases
+
     """
 
 def is_tree_k_balanced(tree: binary_tree_node, k:int ) -> binary_tree_node :
@@ -29,13 +34,13 @@ def is_tree_k_balanced(tree: binary_tree_node, k:int ) -> binary_tree_node :
     :return:
 
     Do a postorder traversal where the recursive function returns the number of nodes and if the
-    tree is k-balanced. On the current node, the tree is k-balanced if BOTH:
-     1) left and right subtrees are k-balaned
+    tree is recursively k-balanced. The current tree is recursively k-balanced if BOTH:
+     1) left and right subtrees are recursively k-balanced
      2) the difference in number of elements between both trees is at most k
 
     keep a global vairable for the binary_binary_tree_node to return. assign the binary tree only
-    if the current node is not k-balanced.
-
+    if the current node is not k-balanced. Avoid doign any more searching if an appropriate node has been
+    recorded.
     """
 
 
--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -2,22 +2,36 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+# def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
+#
+#     # return the height of the sub-tree, o/w -1 if un balanced
+#     def getHeightIfBalanced(node: BinaryTreeNode) -> int :
+#         if not node.left and not node.right :
+#             return 0
+#
+#         lHeight = getHeightIfBalanced(node.left) if node.left else -1
+#         rHeight = getHeightIfBalanced(node.right) if node.right else -1
+#         if lHeight == -2 or rHeight == -2 :
+#             return -2
+#
+#         return 1 + max(lHeight,rHeight) if abs(lHeight-rHeight) <= 1 else -2
+#
+#     return getHeightIfBalanced(tree) >= 0 if tree else True
+
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-
-    # return the height of the sub-tree, o/w -1 if un balanced
-    def getHeightIfBalanced(node: BinaryTreeNode) -> int :
-        if not node.left and not node.right :
-            return 0
-
-        lHeight = getHeightIfBalanced(node.left) if node.left else -1
-        rHeight = getHeightIfBalanced(node.right) if node.right else -1
-        if lHeight == -2 or rHeight == -2 :
+    # return -2 if unbalanced, -1 if node is null, and the height
+    # of the subtree if it is balanced.
+    def getHeightOrUnbalanced(node: BinaryTreeNode) -> int:
+        if not node : return -1
+        lHeight = getHeightOrUnbalanced(node.left)
+        if lHeight == -2 : return -2
+        rHeight = getHeightOrUnbalanced(node.right)
+        if rHeight == -2 : return -2
+        elif -1 <= rHeight - lHeight <= 1:
+            return max(rHeight,lHeight) + 1
+        else:
             return -2
-
-        return 1 + max(lHeight,rHeight) if abs(lHeight-rHeight) <= 1 else -2
-
-    return getHeightIfBalanced(tree) >= 0 if tree else True
-
+    return getHeightOrUnbalanced(tree) != -2
 
 if __name__ == '__main__':
     exit(
