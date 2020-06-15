6e80d0d08f4bbc4a0115160ccaf37c12af491e26
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-15 15:08:02 -0400

    Redo 11.01

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -3,19 +3,32 @@ from typing import List
 from test_framework import generic_test
 
 
+# def search_first_of_k(A: List[int], k: int) -> int:
+#     l, r = 0, len(A) -1
+#     while l <= r :
+#         m = l + (r-l) // 2
+#         if k < A[m] :
+#             r = m - 1
+#         elif A[m] < k :
+#             l = m + 1
+#         else :
+#             r = m
+#             if l == r :
+#                 return r
+#     return -1
+
+# redo
 def search_first_of_k(A: List[int], k: int) -> int:
-    l, r = 0, len(A) -1
-    while l <= r :
-        m = l + (r-l) // 2
-        if k < A[m] :
-            r = m - 1
-        elif A[m] < k :
-            l = m + 1
-        else :
-            r = m
-            if l == r :
-                return r
-    return -1
+   i,j = 0,len(A)-1
+   while i <= j :
+       m = (i+j)//2
+       if A[m] == k and (m==0 or A[m-1] !=k) :
+           return m
+       elif A[m] >= k :
+           j = m-1
+       else :
+           i = m+1
+   return - 1
 
 
 if __name__ == '__main__':
