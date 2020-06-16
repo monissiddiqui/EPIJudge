8d874085907985cd6b58d617fba206939dbc6aa9
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-15 22:01:20 -0400

    Complete 11.04

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def square_root(k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    if k <= 1: return k
+    l,u= 0, k//2
+    m = 0
+    while l <= u and m**2 != k:
+        m = (l+u)//2
+        if m**2 < k :
+            l = m+1
+        elif m**2 > k :
+            u = m-1
+    return m if m**2 <= k else m-1
 
 
 if __name__ == '__main__':
