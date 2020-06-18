285fee4f9d60800030f64316f569d033830c17eb
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-17 21:13:08 -0400

    Redo 14.01

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -3,24 +3,39 @@ from test_framework import generic_test
 from collections import deque
 
 
+# def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
+#     if not tree : return True
+#     if tree.left and tree.left.data > tree.data : return False
+#     if tree.right and tree.right.data < tree.data : return False
+#
+#     prev = float("-inf")
+#     def in_order_traversal(node : BinaryTreeNode) -> bool :
+#         nonlocal prev
+#         if not node : return True
+#         if node.left and not in_order_traversal(node.left) :
+#             return False
+#         if node.data < prev : return False
+#         prev = node.data
+#         if node.right and not in_order_traversal(node.right) :
+#             return False
+#         return True
+#
+#     return in_order_traversal(tree)
+
+# redo question
 def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
-    if not tree : return True
-    if tree.left and tree.left.data > tree.data : return False
-    if tree.right and tree.right.data < tree.data : return False
-
-    prev = float("-inf")
-    def in_order_traversal(node : BinaryTreeNode) -> bool :
-        nonlocal prev
+    prevVal = float("-inf")
+    def in_order(node: BinaryTreeNode) -> bool :
+        nonlocal prevVal
         if not node : return True
-        if node.left and not in_order_traversal(node.left) :
+        is_left_bst =  in_order(node.left)
+        if not is_left_bst : return False
+        if node.data < prevVal :
             return False
-        if node.data < prev : return False
-        prev = node.data
-        if node.right and not in_order_traversal(node.right) :
-            return False
-        return True
+        prevVal = node.data
+        return  in_order(node.right)
+    return in_order(tree)
 
-    return in_order_traversal(tree)
 
 if __name__ == '__main__':
     exit(
