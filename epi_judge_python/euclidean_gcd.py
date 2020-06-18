4df38676039dc3b08265f6c8ab65416e90313047
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-18 13:13:13 -0400

    Redo 15.00 with loops

--- epi_judge_python/euclidean_gcd.py
+++ epi_judge_python/euclidean_gcd.py
@@ -4,6 +4,12 @@ from test_framework import generic_test
 def gcd(x: int, y: int) -> int:
     return x if y==0 else gcd(y,x%y)
 
+# redo problem (without recursive stack)
+def gcd(x: int, y: int) -> int:
+    while (x >0) :
+        y,x = x, y%x
+    return y
+
 
 if __name__ == '__main__':
     exit(generic_test.generic_test_main('euclidean_gcd.py', 'gcd.tsv', gcd))
