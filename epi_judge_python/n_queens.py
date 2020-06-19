1f3fd0463b09e8181e2fe90e89e290f4230586f5
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-19 00:39:40 -0400

    Complete 15.02

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -2,10 +2,34 @@ from typing import List
 
 from test_framework import generic_test
 
+from collections import deque
 
 def n_queens(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    if n == 0 : return []
+    remainingCols = set(range(n)) # c
+    remainingLRDiags = set(range(-n,n))  # c - r
+    remainingRLDiags = set(range(0,2*n-1)) # c + r
+
+    currentQueens = deque([],maxlen=n)
+    successfulPlacements = deque([])
+
+    def findRemainingPlacementsFromRow(r: int) :
+        if len(currentQueens) == n :
+            successfulPlacements.append(list(currentQueens))
+        for c in list(remainingCols) :
+            if c-r in remainingLRDiags and c+r in remainingRLDiags :
+                currentQueens.append(c)
+                remainingRLDiags.remove(c+r)
+                remainingLRDiags.remove(c-r)
+                remainingCols.remove(c)
+                findRemainingPlacementsFromRow(r+1)
+                remainingRLDiags.add(c+r)
+                remainingLRDiags.add(c-r)
+                remainingCols.add(c)
+                currentQueens.pop()
+
+    findRemainingPlacementsFromRow(0)
+    return list(successfulPlacements)
 
 
 def comp(a, b):
