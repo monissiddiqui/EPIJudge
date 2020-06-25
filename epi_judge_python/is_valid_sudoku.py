6221a4ebee1eab103e12ed1235c3d995a3b3ac48
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-24 22:59:22 -0400

    Complete 5.17

--- epi_judge_python/is_valid_sudoku.py
+++ epi_judge_python/is_valid_sudoku.py
@@ -2,10 +2,39 @@ from typing import List
 
 from test_framework import generic_test
 
+import itertools
 
 # Check if a partially filled matrix has any conflicts.
 def is_valid_sudoku(partial_assignment: List[List[int]]) -> bool:
-    # TODO - you fill in here.
+    m = len(partial_assignment)
+    n = len(partial_assignment[0])
+    # checking rows
+    for i in range(m) :
+        numSet = 0
+        for j in partial_assignment[i] :
+            if j == 0 : continue
+            if (1 << j) & numSet :
+                return False
+            numSet |= (1 << j)
+
+    # checking columns
+    for j in range(n) :
+        numSet = 0
+        for i in range(m) :
+            if partial_assignment[i][j] == 0 : continue
+            if (1 << partial_assignment[i][j]) & numSet :
+                return False
+            numSet |= (1 << partial_assignment[i][j])
+
+    # Checking squares
+    for r in (1,4,7):
+        for c in (1,4,7) :
+            numSet = 0
+            for i,j in itertools.product(*[[-1,0,1]]*2) :
+                if partial_assignment[r+i][c+j] == 0: continue
+                if (1 << partial_assignment[r+i][c+j]) & numSet :
+                    return False
+                numSet |= (1 << partial_assignment[r+i][c+j])
     return True
 
 
