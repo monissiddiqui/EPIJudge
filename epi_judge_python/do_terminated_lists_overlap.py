07f42b684a182b9497d57d2e2fb3bbdb16a1039e
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-26 06:30:40 -0400

    Complete 7.04

--- epi_judge_python/do_terminated_lists_overlap.py
+++ epi_judge_python/do_terminated_lists_overlap.py
@@ -7,7 +7,37 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def overlapping_no_cycle_lists(l0: ListNode, l1: ListNode) -> ListNode:
-    # TODO - you fill in here.
+    if not (l0 and l1) : return None
+    n0, n1 = l0, l1
+    o0, o1, = 0,0
+
+    # find intersection point, count extra pointers once end is reached
+    while n0 is not n1 and (n0.next or n1.next ) :
+        if not n0.next :
+            o0 += 1
+        else :
+            n0 = n0.next
+        if not n1.next :
+            o1 += 1
+        else :
+            n1 = n1.next
+    # cases where pointer's reached together or didn't meet at all based on offset values
+    if o0 + o1 == 0:
+        if n0 == n1 :
+            return n1
+        else :
+            return None
+    # for list that accumulated offset, delay iterating on that list for o iterations.
+    s,l,o = (l0,l1,o0) if o0 >0 else (l1,l0,o1)
+    while s is not l :
+        if o >0 :
+            o -=1
+        else :
+            s = s.next
+        l = l.next
+    return s
+
+
     return ListNode()
 
 
