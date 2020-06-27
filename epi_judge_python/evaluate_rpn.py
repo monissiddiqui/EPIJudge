b868a56abab782014d6e1497821df9859775db67
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-27 01:20:30 -0400

    Complete 8.02

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,20 @@
 from test_framework import generic_test
 
+from collections import deque
+from typing import Deque, Callable
+from operator import add, floordiv, mul, sub
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    stack : Deque[int]  =  deque([],maxlen=len(expression))
+    functionOfOperator : Callable = {'+':add, '*':mul, '-':sub , '/':floordiv}
+    for s in expression.split(',') :
+        if s in functionOfOperator :
+            r = stack.pop()
+            l = stack.pop()
+            stack.append(functionOfOperator[s](l,r))
+        else :
+            stack.append(int(s))
+    return stack.pop()
 
 
 if __name__ == '__main__':
