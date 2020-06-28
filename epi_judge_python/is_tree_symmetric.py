7140264295f7bfac935a7819ce92967a63b7262f
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-28 04:29:58 -0400

    Complete 9.02

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -1,10 +1,19 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+from typing import Optional
 
 def is_symmetric(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    def mirrorTraversal(ltree: Optional[BinaryTreeNode], rtree : Optional[BinaryTreeNode]) -> bool :
+        if ltree is None and rtree is None : return True
+        if not ltree or not rtree : return False
+        if  ltree.data ==  rtree.data and \
+            mirrorTraversal(ltree.right,rtree.left) and \
+            mirrorTraversal(ltree.left,rtree.right) :
+                return True
+        return False
+
+    return not tree or mirrorTraversal(tree.left,tree.right)
 
 
 if __name__ == '__main__':
