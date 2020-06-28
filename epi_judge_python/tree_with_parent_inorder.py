38b26ca2d4fb89826f434d1dabcf421aab6f67b1
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-06-28 06:11:58 -0400

    Complete 9.11

--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -3,10 +3,31 @@ from typing import List
 from binary_tree_with_parent_prototype import BinaryTreeNode
 from test_framework import generic_test
 
+from  collections import deque
 
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    if not tree: return []
+    res =  deque()
+    node = tree
+    while node.left :
+        node = node.left
+
+    # add node, then get successor :
+    while node :
+        res.append(node.data)
+
+        # get successor with parent references
+        if node.right :
+            # get left-most of right subtree
+            node = node.right
+            while node.left :
+                node = node.left
+        else :
+            while node.parent and node.parent.right is node :
+                node = node.parent
+            node = node.parent if node.parent else None
+
+    return list(res)
 
 
 if __name__ == '__main__':
