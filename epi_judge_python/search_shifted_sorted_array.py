eb1ae5e4450db173d4195079f2ca08ca649ef38c
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-02 06:21:58 -0400

    Complete 11.03

--- epi_judge_python/search_shifted_sorted_array.py
+++ epi_judge_python/search_shifted_sorted_array.py
@@ -4,8 +4,17 @@ from test_framework import generic_test
 
 
 def search_smallest(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    l,r = 0, len(A) -1
+    m = 0
+    while l <= r :
+        m = (l+r)//2
+        if (m >0 and A[m-1] > A[m]) :
+            return m
+        elif A[r] < A[m] :
+            l = m+1
+        else :
+            r = m-1
+    return m
 
 
 if __name__ == '__main__':
