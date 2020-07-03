e29b131003bc9a9de5d1382974910064095db409
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-03 10:01:28 -0400

    Complete 11.09

--- epi_judge_python/absent_value_array.py
+++ epi_judge_python/absent_value_array.py
@@ -3,15 +3,36 @@ from typing import Iterator
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+import itertools
 
 def find_missing_element(stream: Iterator[int]) -> int:
-    # TODO - you fill in here.
-    return 0
 
+    counter = [0] * (1<<16)
+    MASK = 0xFFFF0000
+    stream,stream_copy = itertools.tee(stream,2)
 
+    for ip in stream :
+        counter[(ip & MASK) >> 16] += 1
+
+    MS_HALF = 0
+    for i,c in enumerate(counter) :
+        if c < 1<<16 :
+            MS_HALF = i << 16
+            break
+    counter = [0] * (1<<16)
+    FILTER_MASK = MASK
+    MASK = 0xFFFF
+    for ip in stream_copy :
+        if FILTER_MASK & ip == MS_HALF :
+            counter[ip & MASK] += 1
+
+    for lower_half,count in enumerate(counter) :
+        if count == 0 :
+            return MS_HALF | lower_half
+    return -1
 def find_missing_element_wrapper(stream):
     try:
-        res = find_missing_element(iter(stream))
+        res = find_missing_element(iter(stream))    
         if res in stream:
             raise TestFailure('{} appears in stream'.format(res))
     except ValueError:
