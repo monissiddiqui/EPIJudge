3f02d36178d3158adb70f646a39f248c1c418c11
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-03 10:35:25 -0400

    Redo 12.05

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -3,14 +3,23 @@ from typing import List
 from test_framework import generic_test
 
 
-def find_nearest_repetition(paragraph: List[str]) -> int:
-    minDistance = float("inf")
-    mostRecentLocation = {}
-    for i,word in enumerate(paragraph) :
-        if word in mostRecentLocation :
-            minDistance = min(i-mostRecentLocation[word], minDistance)
-        mostRecentLocation[word] = i
+# def find_nearest_repetition(paragraph: List[str]) -> int:
+#     minDistance = float("inf")
+#     mostRecentLocation = {}
+#     for i,word in enumerate(paragraph) :
+#         if word in mostRecentLocation :
+#             minDistance = min(i-mostRecentLocation[word], minDistance)
+#         mostRecentLocation[word] = i
+#
+#     return minDistance if minDistance < float("inf") else -1
 
+def find_nearest_repetition(paragraph: List[str]) -> int:
+    lastOccurance = {}
+    minDistance = float("inf")
+    for i,word in enumerate(paragraph) :
+        if word in lastOccurance :
+            minDistance = min(i - lastOccurance[word],minDistance)
+        lastOccurance[word] = i
     return minDistance if minDistance < float("inf") else -1
 
 
