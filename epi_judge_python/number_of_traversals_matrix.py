baeb9326a563471c4b0dcfb31fbf0aa9c301bec2
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-05 12:08:10 -0400

    Complete 16.03

--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -1,10 +1,17 @@
 from test_framework import generic_test
 
+from math import comb
+
+def number_of_ways_efficient(n: int, m: int) -> int:
+    return comb(m+n-2,m-1)
 
 def number_of_ways(n: int, m: int) -> int:
-    # TODO - you fill in here.
-    return 0
-
+    k = min(m,n)
+    t = [1] * k
+    for _ in range(1,max(m,n)) :
+        for j in range(1,k) :
+            t[j] += t[j-1]
+    return t[-1]
 
 if __name__ == '__main__':
     exit(
