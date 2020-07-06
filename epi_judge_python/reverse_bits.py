ff457abd538bf065dbfbfa8fef439bfc66cad9ae
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-06 02:26:42 -0400

    Complete 4.03

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,12 +1,17 @@
 from test_framework import generic_test
 
 
-def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+def reverse_bits_pdac(x: int) -> int:
+    x = (x & 0xFFFFFFFF00000000) >> 32 | (x & 0x00000000FFFFFFFF) << 32
+    x = (x & 0xFFFF0000FFFF0000) >> 16 | (x & 0x0000FFFF0000FFFF) << 16
+    x = (x & 0xFF00FF00FF00FF00) >> 8  | (x & 0x00FF00FF00FF00FF) << 8
+    x = (x & 0xF0F0F0F0F0F0F0F0) >> 4  | (x & 0x0F0F0F0F0F0F0F0F) << 4
+    x = (x & 0xCCCCCCCCCCCCCCCC) >> 2  | (x & 0x3333333333333333) << 2
+    x = (x & 0xAAAAAAAAAAAAAAAA) >> 1  | (x & 0x5555555555555555) << 1
+    return x
 
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('reverse_bits.py', 'reverse_bits.tsv',
-                                       reverse_bits))
+                                       reverse_bits_pdac))
