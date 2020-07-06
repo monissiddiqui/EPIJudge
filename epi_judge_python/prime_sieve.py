11f61cae5d662e40a61990988da6091c6cd36dea
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-06 17:00:36 -0400

    Complete 5.09

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -2,11 +2,21 @@ from typing import List
 
 from test_framework import generic_test
 
+from collections import deque
 
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    isPrime = [True] * (n+1)
+    primes = deque([], maxlen=n)
+    for i in range(2,n+1) :
+        if isPrime[i] :
+            primes.append(i)
+            j = 2 *i
+            while j <= n :
+                isPrime[j] = False
+                j += i
+    return list(primes)
+
 
 
 if __name__ == '__main__':
