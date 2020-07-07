6dd35943cbff3bc57e729ec9094bbb04303d99a8
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-07 11:46:44 -0400

    Complete 8.03

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -1,10 +1,21 @@
 from test_framework import generic_test
 
+from collections import deque
 
 def is_well_formed(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    stack = deque([],maxlen=len(s))
+    # openSymbols = set('{[(')
+    matchingOpenSymbolOf = {'}':'{',']':'[',')':'('}
 
+    for c in s :
+        if c not in matchingOpenSymbolOf :
+            stack.append(c)
+        elif len(stack) == 0 :
+            return False
+        else :
+            if matchingOpenSymbolOf[c] != stack.pop() :
+                return False
+    return len(stack) == 0
 
 if __name__ == '__main__':
     exit(
