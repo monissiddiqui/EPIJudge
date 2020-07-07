582e8610f78484ef659fdefe6c74efa4be1266e1
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-06 20:01:35 -0400

    Complete 6.08

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -1,9 +1,26 @@
 from test_framework import generic_test
 
+from collections import deque
 
 def look_and_say(n: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    num = ["1"]
+    for _ in range(n-1) :
+        newNum = deque([],maxlen=2*len(num))
+        count = 1
+        prev = num[0]
+        for i in range(1,len(num)) :
+            if num[i] == prev :
+                count += 1
+            else :
+                newNum.append(str(count))
+                newNum.append(prev)
+                prev = num[i]
+                count = 1
+        newNum.append(str(count))
+        newNum.append(prev)
+        num = list(newNum)
+
+    return ''.join(num)
 
 
 if __name__ == '__main__':
