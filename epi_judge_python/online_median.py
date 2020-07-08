89bc59d6c3d3ce33e9a3a832dc58e925c269874b
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-08 13:37:50 -0400

    Complete 10.05

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -2,10 +2,40 @@ from typing import Iterator, List
 
 from test_framework import generic_test
 
+import heapq
+from collections import deque
+from statistics import mean
 
 def online_median(sequence: Iterator[int]) -> List[float]:
-    # TODO - you fill in here.
-    return []
+    first = next(sequence,None)
+    if first is None : return []
+    minHeap = [first]
+    maxHeap = [-first]
+    medians = deque([first])
+    isOddElement = False
+    for s in sequence:
+        if s < -maxHeap[0] :
+            if isOddElement :
+                heapq.heappush(maxHeap,-s)
+                heapq.heappush(minHeap,-maxHeap[0])
+            else :
+                -heapq.heapreplace(maxHeap,-s)
+        elif s > minHeap[0] :
+            if isOddElement :
+                heapq.heappush(minHeap,s)
+                heapq.heappush(maxHeap,-minHeap[0])
+            else:
+                heapq.heapreplace(minHeap,s)
+        else :
+            if isOddElement  :
+                heapq.heappush(minHeap,s)
+                heapq.heappush(maxHeap,-s)
+            # otherwise in previous iteration tops of both heaps are equal, so new element is equal to
+            # it and we can ignore it
+        medians.append((-maxHeap[0]+minHeap[0])/2)
+        isOddElement ^= True
+
+    return list(medians)
 
 
 def online_median_wrapper(sequence):
