056f1197896ad81250ce79db44092bcaafc77d9e
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-11 18:35:00 -0400

    Complete 15.09

--- epi_judge_python/sudoku_solve.py
+++ epi_judge_python/sudoku_solve.py
@@ -7,10 +7,54 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+from itertools import product
 
 def solve_sudoku(partial_assignment: List[List[int]]) -> bool:
-    # TODO - you fill in here.
-    return True
+    rowSets = [set(range(1,10)) for _ in range(9)]
+    columnSets = [set(range(1,10)) for _ in range(9)]
+    boxSets = [set(range(1,10)) for _ in range(9)]
+    for r in range(9):
+        for c in range(9) :
+            if partial_assignment[r][c] != 0 :
+                b = 3 * (r // 3) + (c//3)
+                val = partial_assignment[r][c]
+                rowSets[r].remove(val)
+                columnSets[c].remove(val)
+                boxSets[b].remove(val)
+    count = [0]
+    def setNumber(r: int, c : int) -> bool :
+        nonlocal count
+        print(count)
+        count[0] += 1
+        b = 3* (r//3) + (c//3)
+        if partial_assignment[r][c] != 0 :
+            return True
+        for val in range(1,10) :#rowSets[r] & columnSets[c] & boxSets[b] :
+            if val not in rowSets[r] & columnSets[c] & boxSets[b] :
+                continue
+            partial_assignment[r][c] = val
+            rowSets[r].remove(val)
+            columnSets[c].remove(val)
+            boxSets[b].remove(val)
+            isBadValue = False
+            for i in range(9) :
+                if i != r :
+                    if not setNumber(i,c) :
+                        isBadValue = True
+                        break
+                if i != c :
+                    if not setNumber(r,i) :
+                        isBadValue = True
+                        break
+            if isBadValue :
+                partial_assignment[r][c] = 0
+                rowSets[r].add(val)
+                columnSets[c].add(val)
+                boxSets[b].add(val)
+            else :
+                break
+        return partial_assignment[r][c] != 0
+    return all(setNumber(r,c) for r,c in product(range(9),repeat=2))
 
 
 def assert_unique_seq(seq):
@@ -59,6 +103,7 @@ def solve_sudoku_wrapper(executor, partial_assignment):
 
 
 if __name__ == '__main__':
+
     exit(
         generic_test.generic_test_main('sudoku_solve.py', 'sudoku_solve.tsv',
                                        solve_sudoku_wrapper))
