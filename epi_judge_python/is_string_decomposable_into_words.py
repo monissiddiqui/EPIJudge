bf7e6eaac2577544c4c10eaa8e70787b32f0a88f
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-12 04:23:32 -0400

    Complete 16.07

--- epi_judge_python/is_string_decomposable_into_words.py
+++ epi_judge_python/is_string_decomposable_into_words.py
@@ -5,12 +5,26 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+from collections import deque
 
 def decompose_into_dictionary_words(domain: str,
                                     dictionary: Set[str]) -> List[str]:
-    # TODO - you fill in here.
-    return []
-
+    decomposition = deque([],maxlen=len(domain))
+    visited = set()
+    def searchForWordFrom(i : int) -> bool :
+        if i == len(domain) : return True
+        if i in visited : return False
+        visited.add(i)
+        j = i +1
+        while j <= len(domain) :
+            decomposition.append(domain[i:j])
+            if domain[i:j] in dictionary and searchForWordFrom(j) :
+                return True
+            decomposition.pop()
+            j+=1
+        return False
+    searchForWordFrom(0)
+    return list(decomposition)
 
 @enable_executor_hook
 def decompose_into_dictionary_words_wrapper(executor, domain, dictionary,
