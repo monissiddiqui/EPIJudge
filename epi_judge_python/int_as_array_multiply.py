75d3c9dc709069cfd14616a78db46edb1b6571a2
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-13 18:24:01 -0400

    Complete 5.03

--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -4,8 +4,28 @@ from test_framework import generic_test
 
 
 def multiply(num1: List[int], num2: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    res = [0] * (len(num1)+len(num2))
+    sign = 1
+    if num1[0] <0 :
+        num1[0] *= -1
+        sign *= -1
+    if num2[0] < 0 :
+        num2[0] *= -1
+        sign *= -1
+    for i in range(len(num1)) :
+        for j in range(len(num2)) :
+            res[~(i+j)] += num1[~i]*num2[~j]
+            res[~(i+j+1)] += res[~(i+j)] //10
+            res[~(i+j)] %= 10
+
+    i = 0
+    while i < len(res) :
+        if res[i] != 0 :
+            res [i] *= sign
+            break
+        i += 1
+    return res[i:] if i < len(res) else [0]
+
 
 
 if __name__ == '__main__':
