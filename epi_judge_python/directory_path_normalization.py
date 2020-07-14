37ef6a69db71312598528ae5428d23fdebe34714
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-14 03:24:55 -0400

    Complete 8.04

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -1,9 +1,23 @@
 from test_framework import generic_test
 
+from collections import deque
 
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    path = path.split('/')
+    stack = deque([],maxlen=len(path))
+    i = 0
+    root = ""
+    if path[0] == "" :
+        i += 1
+        root = "/"
+
+    nothing = set(["","."])
+    for p in path[i:] :
+        if p == ".." and stack and p != stack[-1]:
+            stack.pop()
+        elif p not in nothing :
+            stack.append(p)
+    return root + '/'.join(stack)
 
 
 if __name__ == '__main__':
