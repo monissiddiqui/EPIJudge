27189a1cc4e516e98ad596504535d9f729a7a6df
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-13 20:43:33 -0400

    Complete 5.15

--- epi_judge_python/random_subset.py
+++ epi_judge_python/random_subset.py
@@ -7,17 +7,35 @@ from test_framework.random_sequence_checker import (
     compute_combination_idx, run_func_with_retries)
 from test_framework.test_utils import enable_executor_hook
 
+from random import randrange
 
 def random_subset(n: int, k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    if k > n//2 : return list(set(range(n))-set(random_subset(n,n-k)))
+    i = 0
+    nums = list(range(n))
+    for i in range(k):
+        j = randrange(n-i) + i
+        nums[i], nums[j] = nums[j], nums[i]
+    return nums[:k]
+
+def random_subset_efficient_space(n :int, k :int) -> List[int] :
+    # if k > n// 2 : return random_subset_efficient_space(n,n-k)
+    selected = {}
+    for i in range(k) :
+        if i not in selected :
+            selected[i] = i
+        j = randrange(i,n)
+        if j not in selected :
+            selected[j] = j
+        selected[j],selected[i] = selected[i],selected[j]
+    return [selected[i] for i in range(k)]
 
 
 @enable_executor_hook
 def random_subset_wrapper(executor, n, k):
     def random_subset_runner(executor, n, k):
         results = executor.run(
-            lambda: [random_subset(n, k) for _ in range(100000)])
+            lambda: [random_subset_efficient_space(n, k) for _ in range(100000)])
 
         total_possible_outcomes = binomial_coefficient(n, k)
         comb_to_idx = {
