ef9676324d365802421914569fb69d470605d62c
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-17 05:15:50 -0400

    Complete 15.10

--- epi_judge_python/gray_code.py
+++ epi_judge_python/gray_code.py
@@ -1,19 +1,51 @@
 import functools
-from typing import List
+from typing import List, Deque
 
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
+from epi_judge_python_solutions.gray_code import  differ_by_1_bit
 
-def gray_code(num_bits: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+from collections import deque
+
+def gray_code_bu(num_bits: int) -> List[int]:
+    if num_bits == 0 : return [0]
+    # grayCodeCache : Dict[int,List[int]] = {0:[]}
+    def getGrayCode(n: int) -> List[int] :
+        if n == 1 : return [0,1]
+        MASK = 1 << n-1
+        bit = MASK
+        res : Deque[int] = deque([],maxlen=2**n)
+        for num in getGrayCode(n-1) :
+            res.extend([bit | num, bit^ MASK | num])
+            bit ^= MASK
+        return list(res)
+    return getGrayCode(num_bits)
+
+# does not work
+def gray_code_td(num_bits: int) -> List[int]:
+    if num_bits == 0 : return [0]
+    num = 0
+    res : Deque[int] = deque([],maxlen=2**num_bits)
+    def constructGrayCode(n : int) -> None :
+        nonlocal num
+        if n == num_bits :
+            res.append(num)
+            return
+        MASK = 1 << n
+        constructGrayCode(n+1)
+        num ^= MASK
+        constructGrayCode(n+1)
+    constructGrayCode(0)
+    return list(res)
+
+
 
 
 @enable_executor_hook
 def gray_code_wrapper(executor, num_bits):
-    result = executor.run(functools.partial(gray_code, num_bits))
+    result = executor.run(functools.partial(gray_code_td, num_bits))
 
     expected_size = (1 << num_bits)
     if len(result) != expected_size:
