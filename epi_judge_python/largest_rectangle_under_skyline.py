a90907d2f3d308fad11e3d8a699c0cacfff040e1
Monis Siddiqui <monisqsiddiqui@gmail.com>
2020-07-19 09:46:07 -0400

    Complete 17.08

--- epi_judge_python/largest_rectangle_under_skyline.py
+++ epi_judge_python/largest_rectangle_under_skyline.py
@@ -1,11 +1,23 @@
-from typing import List
+from typing import List, Deque, Tuple
 
 from test_framework import generic_test
 
+from collections import deque
 
 def calculate_largest_rectangle(heights: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    incrQ : Deque[int]= deque([],maxlen=len(heights))
+    maxArea = 0
+    for i in range(len(heights) +1 ) :
+        val = 0 if i == len(heights) else heights[i]
+        while  incrQ and  heights[incrQ[-1]] >= val:
+            pillar = incrQ.pop()
+            if heights[pillar] != val :
+                l = incrQ[-1] if incrQ else -1
+                maxArea = max(maxArea, heights[pillar]*(i-l-1))
+
+        if i < len(heights):
+            incrQ.append(i)
+    return maxArea
 
 
 if __name__ == '__main__':
